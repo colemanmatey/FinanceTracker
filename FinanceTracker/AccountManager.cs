da1fdb2128bb2435abb109875c465114eb233ae4
Coleman A. Matey <113218526+colemanmatey@users.noreply.github.com>
2023-12-04 07:56:25 +0000

    Update AccountManager.cs

--- FinanceTracker/AccountManager.cs
+++ FinanceTracker/AccountManager.cs
@@ -9,7 +9,6 @@ public class AccountManager
     private List<Account> accounts;
     private const string FileName = "accounts.json";
 
-
     public AccountManager()
     {
         // Create a list of accounts
@@ -18,37 +17,23 @@ public class AccountManager
 
     public void AddAccount(Account account)
     {
-        // Get list of all accounts from file
-        accounts = LoadAccounts();
-
         // Add new account to the list
         accounts.Add(account);
-
-        // Save the new list back to the file
-        SaveAccounts(accounts);
-
     }
     public void RemoveAccount(Account account)
     {
-        // Get list of all accounts from file
-        accounts = LoadAccounts();
-
         // Remove new account from the list
         accounts.Remove(account);
-
-        // Save the new list back to the file
-        SaveAccounts(accounts);
-
     }
 
-    private void SaveAccounts(List<Account> accounts)
+    public void SaveAccounts()
     {
         // Serialize accounts to JSON and save to file
         string json = JsonSerializer.Serialize(accounts);
         File.WriteAllText(FileName, json);
     }
 
-    private List<Account> LoadAccounts()
+    public List<Account> LoadAccounts()
     {
         // Deserialize accounts from JSON file
         // if (File.Exists(FileName)){
